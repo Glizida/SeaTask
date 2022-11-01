5094cb47e62f88d69af2e47e8a3c84da5e3e0a2a
Vladislav Gaidukevich <vladislav.gajdukevich@gmail.com>
2022-11-01 21:14:45 +0300

    Включения запросов между источниками (CORS) для отладки

--- WebAPISeaTask/Program.cs
+++ WebAPISeaTask/Program.cs
@@ -3,7 +3,18 @@ using WebAPISeaTask;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var MyAllowSpecificOrigins = "_MyAllowSpecificOrigins";
 // Add services to the container.
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://192.168.100.3:8080");
+                      });
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -20,6 +31,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
 
